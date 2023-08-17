2199e9d63f8470b59ec7166fe9b74b8176bb830f
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-08-17 14:08:00 -0300

    adding main class

---
+++ seafood-queues/src/main/java/com/icaroerasmo/seafood/SeaFoodApplication.java
@@ -0,0 +1,18 @@
+package com.icaroerasmo.seafood;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Log4j2
+@EnableKafka
+@SpringBootApplication
+@EnableReactiveMongoRepositories
+public class SeaFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaFoodApplication.class, args);
+	}
+}
