22110d6d32a86c4191f0e7b7ee75a1131ef3f1e0
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-08-18 16:24:34 -0300

    refactor

--- seafood-core/src/test/java/com/icaroerasmo/seafood/core/CoreTests.java
+++ seafood-core/src/test/java/com/icaroerasmo/seafood/core/CoreTests.java
@@ -1,9 +1,11 @@
 package com.icaroerasmo.seafood.core;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class CoreTests {
 
 	@Test
