8b7d5496bd08e9c796f9e4ca308693f5a4e10ae6
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-08-21 10:53:58 -0300

    adds createdAt and updatedAt to graphql responses

--- seafood-api/src/main/java/com/icaroerasmo/seafood/api/config/GraphQlConfig.java
+++ seafood-api/src/main/java/com/icaroerasmo/seafood/api/config/GraphQlConfig.java
@@ -4,8 +4,7 @@ import com.icaroerasmo.seafood.core.enums.PersonType;
 import com.icaroerasmo.seafood.core.model.Person;
 import graphql.TypeResolutionEnvironment;
 import graphql.scalars.ExtendedScalars;
-import graphql.schema.GraphQLObjectType;
-import graphql.schema.TypeResolver;
+import graphql.schema.*;
 import graphql.schema.idl.EnumValuesProvider;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.TypeRuntimeWiring;
@@ -13,19 +12,56 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Locale;
+
 @Configuration
 public class GraphQlConfig {
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return wiringBuilder -> wiringBuilder.
-                scalar(ExtendedScalars.DateTime).
                 scalar(ExtendedScalars.GraphQLLong).
-                type(TypeRuntimeWiring.newTypeWiring("Person", typeBuilder -> typeBuilder.typeResolver(new TypeResolver() {
+                scalar(localDateTimeScalarType()).
+                type(personTypeWiring());
+    }
+
+    public TypeRuntimeWiring personTypeWiring() {
+        return TypeRuntimeWiring.newTypeWiring("Person",
+                typeBuilder -> typeBuilder.typeResolver(
+                        new TypeResolver() {
+                            @Override
+                            public GraphQLObjectType getType(TypeResolutionEnvironment env) {
+                                final String personType = (String) env.getArguments().get("personType");
+                                return (GraphQLObjectType) env.getSchema().getType(personType);
+                            }
+                        }
+                    ));
+    }
+
+    public GraphQLScalarType localDateTimeScalarType() {
+        return GraphQLScalarType.newScalar()
+                .name("LocalDateTime")
+                .description("LocalDateTime scalar")
+                .coercing(new Coercing() {
                     @Override
-                    public GraphQLObjectType getType(TypeResolutionEnvironment env) {
-                        final String personType = (String) env.getArguments().get("personType");
-                        return (GraphQLObjectType) env.getSchema().getType(personType);
+                    public String serialize(Object input) {
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.ENGLISH);
+                        return formatter.format((LocalDateTime)input);
                     }
-                })));
+
+                    @Override
+                    public Object parseValue(Object o) throws CoercingParseValueException {
+                        return o;
+                    }
+
+                    @Override
+                    public Object parseLiteral(Object o) throws CoercingParseLiteralException {
+                        return o.toString();
+                    }
+                })
+                .build();
     }
 }
--- seafood-core/src/main/java/com/icaroerasmo/seafood/core/model/DocumentBase.java
+++ seafood-core/src/main/java/com/icaroerasmo/seafood/core/model/DocumentBase.java
@@ -18,9 +18,9 @@ public class DocumentBase {
     @Id
     protected String id;
     @CreatedDate
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS")
     protected LocalDateTime createdAt;
     @LastModifiedDate
-    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS")
     protected LocalDateTime updatedAt;
 }
