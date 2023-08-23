1d852f56da85d5a5a5b4005b1f75a68ccc927992
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-08-23 02:32:56 -0300

    rest implementations for query by email and documentNo

--- seafood-rest-api/src/main/java/com/icaroerasmo/seafood/api/rest/controller/UserRestController.java
+++ seafood-rest-api/src/main/java/com/icaroerasmo/seafood/api/rest/controller/UserRestController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RequestMapping("/api/rest/user")
 @org.springframework.web.bind.annotation.RestController
 public class UserRestController extends RestController<User> {
-
+    @GetMapping("/byDocumentNo/{documentNo}")
+    public Mono<User> findUserByDocumentNo(@PathVariable String documentNo) {
+        return((UserService) service).findUserByDocumentNo(documentNo);
+    }
+    @GetMapping("/byEmail/{email}")
+    public Mono<User> findUserByEmail(@PathVariable String email) {
+        return((UserService) service).findUserByEmail(email);
+    }
     @GetMapping("/byNamePrefix/{namePrefix}")
     public Flux<User> findAllStoresByNamePrefix(@PathVariable String namePrefix) {
         return ((UserService) service).findAllUsersByNamePrefix(namePrefix);
