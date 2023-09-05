1fe7b69ada1b253d7fc55236037c9f73499c0dc0
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-09-05 13:44:11 -0300

    refactor

--- seafood-business/src/main/java/com/icaroerasmo/seafood/business/properties/SeafoodProperties.java
+++ seafood-business/src/main/java/com/icaroerasmo/seafood/business/properties/SeafoodProperties.java
@@ -1,16 +1,18 @@
 package com.icaroerasmo.seafood.business.properties;
 
 import lombok.Data;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 @Data
+@Log4j2
 @Configuration
 @ConfigurationProperties(prefix = "seafood")
 public class SeafoodProperties {
-    private CacheProperties cache = new CacheProperties();
+    private final CacheProperties cache = new CacheProperties();
     @Bean
     @ConfigurationProperties(prefix = "cache")
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
